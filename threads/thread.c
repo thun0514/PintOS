e961cefba3c3c6d5d6396f153a91f8463e2c6c5a
Arklimits <zaqokm2@gmail.com>
2024-04-26 12:34:16 +0000

    thread.c: thread_init에 sleep_list 초기화 추가

--- threads/thread.c
+++ threads/thread.c
@@ -111,6 +111,9 @@ void thread_init(void) {
     list_init(&ready_list);
     list_init(&destruction_req);
 
+    /** #Alarm Clock sleep list 초기화 */
+    list_init(&sleep_list);
+
     /* Set up a thread structure for the running thread. */
     initial_thread = running_thread();
     init_thread(initial_thread, "main", PRI_DEFAULT);
