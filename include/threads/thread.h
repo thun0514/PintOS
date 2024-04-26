fe523ff93d08d4cac3530c5a7892b2f83519e85f
Arklimits <zaqokm2@gmail.com>
2024-04-26 13:05:29 +0000

    thread.h: get_next_tick_to_awake함수 삭제

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -119,7 +119,6 @@ extern bool thread_mlfqs;
 void thread_sleep(int64_t ticks);
 void thread_awake(int64_t ticks);
 void update_next_tick_to_awake(int64_t ticks);
-int64_t get_next_tick_to_awake(void);
 
 void thread_init(void);
 void thread_start(void);
