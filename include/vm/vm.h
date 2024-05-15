feb7666d822e1910848d00decb27bcdbb3c65d4e
Won-n-only <siliver12@gmail.com>
2024-05-16 04:32:58 +0900

    feat: Implement Supplemental Page Table

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -86,7 +86,9 @@ struct page_operations {
 /* Representation of current process's memory space.
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
-struct supplemental_page_table {};
+struct supplemental_page_table {
+	struct hash spt_hash;
+};
 
 #include "threads/thread.h"
 void supplemental_page_table_init(struct supplemental_page_table *spt);
