79467639827c29965b9976bc94aa34ef93419a40
Won-n-only <siliver12@gmail.com>
2024-05-27 18:24:26 +0900

    feat: anon.c - swap in/out 구현
    Co-authored-by: thun0514 <thun0514@users.noreply.github.com>

--- vm/anon.c
+++ vm/anon.c
@@ -44,11 +44,35 @@ bool anon_initializer(struct page *page, enum vm_type type, void *kva) {
 /* Swap in the page by read contents from the swap disk. */
 static bool anon_swap_in(struct page *page, void *kva) {
     struct anon_page *anon_page = &page->anon;
+    disk_sector_t sec_no = anon_page->sec_no;
+    if (!bitmap_test(bitmap, sec_no))
+        return NULL;
+
+    for (int i = 0; i < 8; i++)
+        disk_read(swap_disk, sec_no * 8 + i, kva + DISK_SECTOR_SIZE * i);
+
+    bitmap_reset(bitmap, sec_no);
+
+    return true;
 }
 
 /* Swap out the page by writing contents to the swap disk. */
 static bool anon_swap_out(struct page *page) {
     struct anon_page *anon_page = &page->anon;
+    // return true;
+    size_t sec_num = bitmap_scan_and_flip(bitmap, 0, 1, 0);
+    if (sec_num == BITMAP_ERROR)
+        return false;
+    disk_sector_t sec_no = sec_num;
+
+    for (int i = 0; i < 8; i++)  
+        disk_write(swap_disk, sec_no * 8 + i, page->va + DISK_SECTOR_SIZE * i);
+
+    anon_page->sec_no = sec_no;
+    page->frame = NULL;
+
+    pml4_clear_page(thread_current()->pml4, page->va);
+    return true;
 }
 
 /* Destroy the anonymous page. PAGE will be freed by the caller. */
